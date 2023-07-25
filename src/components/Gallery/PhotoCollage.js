3afd58a2c97e54997475fdd01d9faed62d833e2a
KajaBR <jndns@wp.pl>
2023-07-25 10:28:31 +0200

    gallery

---
+++ src/components/Gallery/PhotoCollage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./PhotoCollageStyle.css";
+
+const PhotoCollage = () => {
+  return (
+    <div className="collage">
+      <div className="container">
+        <div className="content"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoCollage;
